fde1adbb761a9f369827032317452e125f098fc7
Daniel Sadig <65622283+SadigDaniel@users.noreply.github.com>
2022-08-08 16:41:49 -0400

    updated weather app
    updated weather app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, {useState} from 'react'
 import axios from 'axios';
 function App() {
-  
+  const [data, setData] = useState({});
+  const [locatio,setLocation] = useState('')
   //const APIkey = "a4b9cf56236de1f7087a9c7a5e4e29ef"
   //const url = `https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=a4b9cf56236de1f7087a9c7a5e4e29ef`
   //`https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=a4b9cf56236de1f7087a9c7a5e4e29ef`
@@ -11,6 +12,26 @@ function App() {
         <div className="top"> 
           <div className='location'> 
             <p>Dallas 123</p>
+          </div>
+          <div className="temp">
+            <h1>65°F</h1>
+          <div className='descreption'>
+            <p> clouds</p>
+          </div>
+          </div>
+          <div className='bottom'> 
+            <div className='feels'>
+              <p className='bold'>65°F</p>
+              <p>Feels Like </p>
+            </div>
+            <div className='humidity'>
+              <p className='bold'>20%</p>
+              <p>Humidity</p>
+            </div>
+            <div className='winds'>
+              <p className='bold'>12 mph</p>
+              <p>Wind</p>
+            </div>
           </div>
         </div>
       </div>
