61c6d9f9773ad7dde3a3c739d4d8ff03bb84f684
Daniel Sadig <65622283+SadigDaniel@users.noreply.github.com>
2022-08-10 23:51:14 -0400

    Updated api call
    updated api call

--- src/App.js
+++ src/App.js
@@ -2,30 +2,52 @@ import React, {useState} from 'react'
 import axios from 'axios';
 function App() {
   const [data, setData] = useState({});
-  const [locatio,setLocation] = useState('')
-  //const APIkey = "a4b9cf56236de1f7087a9c7a5e4e29ef"
-  //const url = `https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=a4b9cf56236de1f7087a9c7a5e4e29ef`
+  const [location,setLocation] = useState('')
+  const APIkey = "a4b9cf56236de1f7087a9c7a5e4e29ef"
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${APIkey}`
   //`https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=a4b9cf56236de1f7087a9c7a5e4e29ef`
+
+    const onSearch = (event) => {
+      if(event.key === 'Enter'){
+      axios.get(url).then((response) => {
+        setData(response.data)
+        console.log(response.data)
+      })
+    }
+    }
+
   return (
     <div className="app">
+      <div className='search'>
+        <input
+        value={location}
+        onChange={(event) => setLocation(event.target.value)}
+        onKeyPress={onSearch}
+        placeholder="Enter location"
+        type="text"
+        />
+      </div>
       <div className='container'>
         <div className="top"> 
           <div className='location'> 
-            <p>Dallas 123</p>
+            <p>{data?.name}</p>
           </div>
           <div className="temp">
-            <h1>65°F</h1>
+            {data?.main !== null ? 
+            <h1>{Math.round(data?.main?.temp - 273.15) } °C</h1> : 
+            <h1>NA </h1>}
           <div className='descreption'>
             <p> clouds</p>
           </div>
           </div>
           <div className='bottom'> 
             <div className='feels'>
-              <p className='bold'>65°F</p>
+              
+              <p className='bold'>{Math.round(data?.main?.feels_like - 273.15)}°F</p>
               <p>Feels Like </p>
             </div>
             <div className='humidity'>
-              <p className='bold'>20%</p>
+              <p className='bold'>{data?.main?.humidity}%</p>
               <p>Humidity</p>
             </div>
             <div className='winds'>
