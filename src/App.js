296a6173133dff168f4b9fbdc4766c0bb376b39e
Daniel Sadig <65622283+SadigDaniel@users.noreply.github.com>
2022-08-13 22:49:38 -0400

    Auto set city and loading
    The app will automatically be set to your city and added a loading gif on all api calls

--- src/App.js
+++ src/App.js
@@ -1,20 +1,63 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import axios from 'axios';
+import loading from './asset/loading.gif'
 function App() {
   const [data, setData] = useState({});
   const [location,setLocation] = useState('')
+  const [latitude, setLatitude] = useState('');
+  const [longitude, setLongitude] = useState('');
+  const [loading, setLoading] = useState(true);
   const APIkey = "a4b9cf56236de1f7087a9c7a5e4e29ef"
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${APIkey}`
+  const urlLongLat = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=a4b9cf56236de1f7087a9c7a5e4e29ef`
   //`https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=a4b9cf56236de1f7087a9c7a5e4e29ef`
 
+    //to-do: move this to an HTTPClient class for all api calls
+    //to-do: create redux store and update loadng and response state from there
     const onSearch = (event) => {
       if(event.key === 'Enter'){
-      axios.get(url).then((response) => {
+        setLoading(true)
+        axios.get(url).then((response) => {
+          setData(response.data)
+          console.log(response.data)
+          setLoading(true)
+        })
+        .catch(function (error) {
+          //On Failed call
+          setLoading(false)
+          console.log("error")
+        })
+      }
+    }
+
+    useEffect(() => {
+      if(latitude !== '' && longitude !== "" && location === ''){
+        setLoading(true)
+      axios.get(urlLongLat)
+      .then((response) => {
+        //On sucsessfull call
         setData(response.data)
         console.log(response.data)
       })
+      .catch(function (error) {
+        //On Failed call
+        setLoading(false)
+        console.log("error")
+      })
     }
-    }
+    }, [latitude, longitude])
+
+    useEffect(() => {
+      if ("geolocation" in navigator) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+          setLatitude(position.coords.latitude)
+          setLongitude(position.coords.longitude)
+          console.log("here")
+        });
+      }
+      
+  
+    })
 
   return (
     <div className="app">
@@ -33,17 +76,19 @@ function App() {
             <p>{data?.name}</p>
           </div>
           <div className="temp">
-            {data?.main !== null ? 
+            {!loading ? 
             <h1>{Math.round(data?.main?.temp - 273.15) } °C</h1> : 
-            <h1>NA </h1>}
+            <img style={{width: '30%', height: '20%', left: "10%"}} src={require('./asset/loading.gif')} alt="loading-gif" />}
           <div className='descreption'>
             <p> clouds</p>
           </div>
           </div>
           <div className='bottom'> 
             <div className='feels'>
-              
-              <p className='bold'>{Math.round(data?.main?.feels_like - 273.15)}°F</p>
+              {!loading ?
+              <p className='bold'>{Math.round(data?.main?.feels_like - 273.15)}°F</p> : 
+              <img className="loadingGif" src={require('./asset/loading.gif')} alt="loading-gif" />
+              }
               <p>Feels Like </p>
             </div>
             <div className='humidity'>
